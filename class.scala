2993453d30de5d65cb770f49756b5ae5d44cd664
nunescarol <59810994+nunescarol@users.noreply.github.com>
2022-10-14 10:31:47 -0300

    Update class.scala

--- class.scala
+++ class.scala
@@ -1,45 +1,27 @@
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.StringIndexer
+  GNU nano 5.4                                           class.scala                                                    import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.attribute.Attribute
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer}
 
-// Load training data
+// Load pt7 data
 val pt7_sparse = {
-	spark.read
-	.format("parquet")
-	.load("hdfs://master:8020/big-data/pt7-hash.parquet")
-	.withColumnRenamed("_1", "label")
-	.withColumnRenamed("_2", "url")
-	.withColumnRenamed("_3", "words")
+        spark.read
+        .format("parquet")
+        .load("hdfs://master:8020/bigdata/pt7-hash.parquet")
+        .withColumnRenamed("_1", "label")
+        .withColumnRenamed("_2", "url")
+        .withColumnRenamed("_3", "words")
+  }
 
-val indexer = new StringIndexer()
-  .setInputCol("label")
-  .setOutputCol("labelNumeric")
+val indexer = new StringIndexer().setInputCol("label").setOutputCol("labelIndex")
 
 val indexed = indexer.fit(pt7_sparse).transform(pt7_sparse)
 
-indexed.show()
+val training_set = indexed.drop("label").withColumnRenamed("labelIndex", "label")
 
-indexed.getClass()
-
-val lr = new LogisticRegression()
-  .setMaxIter(10)
-  .setRegParam(0.3)
-  .setElasticNetParam(0.8)
+val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
 
 // Fit the model
-val lrModel = lr.fit(training)
+val lrModel = lr.fit(training_set)
 
 // Print the coefficients and intercept for logistic regression
-println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-// We can also use the multinomial family for binary classification
-val mlr = new LogisticRegression()
-  .setMaxIter(10)
-  .setRegParam(0.3)
-  .setElasticNetParam(0.8)
-  .setFamily("multinomial")
-
-val mlrModel = mlr.fit(training)
-
-// Print the coefficients and intercepts for logistic regression with multinomial family
-println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
-println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
+println(s"Coefficients: ${lrModel.coefficientMatrix} Intercept: ${lrModel.interceptVector}")
