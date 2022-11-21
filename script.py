ffbb87a07d191c000707a04acc686f7194332dca
Carol Nunes <mcarolinalnunes@gmail.com>
2022-11-21 20:16:51 -0300

    subprocess arg

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ with open('plan.txt') as f:
         command = 'spark-shell --master spark://master:7077 -i class.scala --conf \"spark.shuffle.file.buffer='+parametro[0]+'k\" --conf \"spark.io.compression.lz4.blockSize='+parametro[1]+'k\" --conf \"spark.sql.files.maxPartitionBytes='+parametro[2]+'k\" --conf \"spark.sql.shuffle.partitions='+parametro[3]+'\" --conf \"spark.reducer.maxSizeInFlight='+parametro[4]+'k\" --conf \"spark.default.parallelism='+parametro[5]+'\" --conf \"spark.broadcast.blockSize='+parametro[6]+'k\"'
 
         start_time = time.time()
-        subprocess.run(command)
+        subprocess.run(command, shell=True)
         end_time = time.time()
 
         execution_time = end_time-start_time
