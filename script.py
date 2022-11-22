3e93c23dd724f4f38e74fa3837c4d94c296982d8
Carol Nunes <mcarolinalnunes@gmail.com>
2022-11-22 12:38:34 -0300

    append

--- script.py
+++ script.py
@@ -9,17 +9,12 @@ with open('plan.txt') as f:
     for element in vetor:
         parametro = element.split(',')
 
-        
-        command = 'spark-submit --master spark://master:7077 class.scala --conf \"spark.shuffle.file.buffer='+parametro[0]+'k\" --conf \"spark.io.compression.lz4.blockSize='+parametro[1]+'k\" --conf \"spark.sql.files.maxPartitionBytes='+parametro[2]+'k\" --conf \"spark.sql.shuffle.partitions='+parametro[3]+'\" --conf \"spark.reducer.maxSizeInFlight='+parametro[4]+'k\" --conf \"spark.default.parallelism='+parametro[5]+'\" --conf \"spark.broadcast.blockSize='+parametro[6]+'k\"'
 
+        command = 'spark-shell --master spark://master:7077 -i class.scala --conf \"spark.shuffle.file.buffer='+parametro[0]+'k\" --conf \"spark.io.compression.lz4.blo>
         start_time = time.time()
         subprocess.run(command, shell=True)
         end_time = time.time()
 
         execution_time = end_time-start_time
-        with open('times.txt') as f:
-            f.write(execution_time+'\n')
-
-
-
-
+        with open('times.txt', 'w') as f:
+            f.write(str(execution_time)+'\n')
